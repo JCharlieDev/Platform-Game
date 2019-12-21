90b8c30f1fc845f623aac079c60827bbc430731a
JCharles246 <jose.munoz34@uabc.edu.mx>
2019-12-20 20:20:57 -0800

    Skybox and Audio
    Added a skybox and some audio, a little rotating script too.

---
+++ Platform Game/Assets/Scripts/GemRotate.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GemRotate : MonoBehaviour
+{
+    public int rotateSpeed = 2;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //  4th parameter will rotate relative to the world.
+        transform.Rotate(0, rotateSpeed * Time.deltaTime, 0, Space.World);
+
+        
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        gameObject.SetActive(false);
+    }
+
+}
